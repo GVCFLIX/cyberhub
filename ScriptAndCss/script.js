4a385dc394e2382cac7ea21da811675b7fe0e21c
Min Khant (M-Flix) <87593776+GVCFLIX@users.noreply.github.com>
2022-04-03 11:18:30 +0630

    Add files via upload

--- ScriptAndCss/script.js
+++ ScriptAndCss/script.js
@@ -3,3 +3,7 @@ let date = new Date();
 let year = date.getFullYear();
 // console.log(year);
 document.getElementById("autoyear").textContent = year;
+
+
+//window alert
+window.alert("ဆရာကြီးများအား ဒီနေရာကနေ ရှိခိုးပါ၏🙏 အားလုံး ကျန်းမာချမ်းသာကြပါစေ :3🤣 ချီးကားလဲအတိုက်မခံနိုင်ပါ Zzzz....");
